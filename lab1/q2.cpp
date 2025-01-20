d8c3aaae217d58adeae2924d02af79cd7d92f82b
abeeha binte aamer <abeehakhan50@gmail.com>
2025-01-20 10:43:45 +0500

    Update q2.cpp

--- lab1/q2.cpp
+++ lab1/q2.cpp
@@ -1,3 +1,17 @@
+/*Q2: Design a C++ program to manage student marks. Allow the user to input
+marks for students in three subjects (Mathematics, English, and Science). The
+program should calculate the total marks, average marks, and display the grade
+for each student. The user can specify the number of students and then input
+the marks for each subject for each student. Finally, display the marks, total,
+average, and grade for each student. Assume a grading system with the
+following criteria:
+90 or above: Grade A
+80-89: Grade B
+70-79: Grade C
+60-69: Grade D
+Below 60: Grade F*/
+
+
 #include <iostream>
 using namespace std;
 
