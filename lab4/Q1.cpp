4d1f881104948290942f6a2175d9df8470fcb064
abeeha binte aamer <abeehakhan50@gmail.com>
2025-02-10 08:51:45 +0500

    Create Q1.cpp

---
+++ lab4/Q1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+class Student {
+private:
+    int studentId;
+    string name;
+    int age;
+    char grade;
+public:
+    Student() {
+        studentId = 0;
+        age = 0;
+        grade = 'N';
+        name = "";
+    }
+    Student(int id, int age1, char grade1, string n) {
+        studentId = id;
+        age = age1;
+        grade = grade1;
+        name = n;
+    }
+    int getAge() {
+        return age;
+    }
+    string getName() {
+        return name;
+    }
+    char getGrade() {
+        return grade;
+    }
+    int getId() {
+        return studentId;
+    }
+    void isEligibleForScholarship() {
+        if (grade == 'A') {
+            cout << "Eligible for Scholarship" << endl;
+        } else {
+            cout << "Not Eligible for Scholarship" << endl;
+        }
+    }
+    void displayDetails() {
+        cout << "Name: " << getName() << endl;
+        cout << "Student Id: " << getId() << endl;
+        cout << "Age: " << getAge() << endl;
+        cout << "Grade: " << getGrade() << endl;
+    }
+};
+
+int main() {
+    Student object(240940, 20, 'A', "Abeeha");
+    object.displayDetails();
+    object.isEligibleForScholarship();
+    cout << endl;
+    Student objectA(240674, 21,  'D' ,"Neha");
+    objectA.displayDetails();
+    objectA.isEligibleForScholarship();
+}
