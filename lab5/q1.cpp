fe0fd8b5ab38131cef665c346955d083ad3fc574
abeeha binte aamer <abeehakhan50@gmail.com>
2025-03-03 09:34:07 +0500

    Update q1.cpp

--- lab5/q1.cpp
+++ lab5/q1.cpp
@@ -1,3 +1,17 @@
+/*Create a class called Square with the following attributes:
+• sideLength – float variable
+• area – float variable
+• allareas – static float variable
+Create the following methods for your class:
+• Constructors (default and parameterized (only takes sideLength as input))
+• Function to calculate area, the calculated area should always be added to the allareas
+variable each time an area is calculated. (assume it’s called only once for each object)
+• Getters and Setters for all variables
+In your main functions create 3 Squares with different sideLength. Call the area method for each of
+those Square objects. After each time the area method is called, call that square’s getters for area and
+allareas to display the updated values.*/
+
+
 #include<iostream>
 using namespace std;
 
