510897c03fc5bb557072e858381cfb318149ff7f
abeeha binte aamer <abeehakhan50@gmail.com>
2025-03-03 23:15:19 +0500

    Create q5.cpp

---
+++ lab5/q5.cpp
@@ -0,0 +1,7 @@
+/*Keeping in mind our previous car example, write a class that represents a car, and use aggregation and 
+composition to combine different components like the engine, wheels, headlights and steering to create the 
+car object.
+
+Hint: create the individual classes first before performing the composition. Remember 
+that for aggregation, you will need pointers! You’ll be needing constructors and
+*/
